c098159d7280c8c84a94c278b6e69dfca28e3024
Robin Hood <yoga3911@gmail.com>
2022-03-25 22:17:37 +0700

    update home page with integration to firebase

--- lib/app/views/main/home/widgets/product.dart
+++ lib/app/views/main/home/widgets/product.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:provider/provider.dart';
 import 'package:shimmer/shimmer.dart';
 
+import '../../../../view_model/category_provider.dart';
+import '../../../../models/product_model.dart';
 import '../../../../constant/collection.dart';
 import '../../../../constant/color.dart';
 import 'card.dart';
@@ -13,15 +16,33 @@ class Product extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Size _size = MediaQuery.of(context).size;
-
+    final categoryProvider = Provider.of<CategoryProvider>(context);
     return FutureBuilder<QuerySnapshot>(
-      future: MyCollection.products.orderBy("create_at").get(),
+      future: (categoryProvider.getCategory == "Popular")
+          ? MyCollection.products.get()
+          : (categoryProvider.getCategory == "Bread")
+              ? MyCollection.products.where("category_id", isEqualTo: "1").get()
+              : (categoryProvider.getCategory == "Cookies")
+                  ? MyCollection.products
+                      .where("category_id", isEqualTo: "3")
+                      .get()
+                  : (categoryProvider.getCategory == "Cakes")
+                      ? MyCollection.products
+                          .where("category_id", isEqualTo: "4")
+                          .get()
+                      : (categoryProvider.getCategory == "Pastry")
+                          ? MyCollection.products
+                              .where("category_id", isEqualTo: "5")
+                              .get()
+                          : MyCollection.products
+                              .where("category_id", isEqualTo: "6")
+                              .get(),
       builder: (_, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return MasonryGridView.count(
             physics: const NeverScrollableScrollPhysics(),
             shrinkWrap: true,
-            itemCount: 8,
+            itemCount: 6,
             mainAxisSpacing: 20,
             crossAxisSpacing: 20,
             crossAxisCount: 2,
@@ -30,25 +51,63 @@ class Product extends StatelessWidget {
                 baseColor: MyColor.grey,
                 highlightColor: MyColor.grey2,
                 direction: ShimmerDirection.ltr,
-                child: ProductCard(
-                  size: _size,
-                  index: index,
+                child: Container(
+                  height:
+                      index.isOdd ? _size.height * 0.24 : _size.height * 0.3,
+                  decoration: BoxDecoration(
+                    color: MyColor.yellow,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
                 ),
               );
             },
           );
         }
+        if (snapshot.data!.docs.isEmpty) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/icons/not_found.png"),
+                  const SizedBox(height: 20,),
+                  const Text(
+                    "Tidak ada data",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
         return MasonryGridView.count(
           physics: const NeverScrollableScrollPhysics(),
           shrinkWrap: true,
-          itemCount: 8,
+          itemCount: snapshot.data!.docs.length,
           mainAxisSpacing: 20,
           crossAxisSpacing: 20,
           crossAxisCount: 2,
           itemBuilder: (_, index) {
-            return ProductCard(
-              size: _size,
-              index: index,
+            final product = ProductModel.fromJson(
+                snapshot.data!.docs[index].data() as Map<String, dynamic>);
+            return GestureDetector(
+              onTap: () => Navigator.pushNamed(
+                context,
+                "/product",
+                arguments: {
+                  "product_id": snapshot.data!.docs[index].id,
+                  "product_data": product
+                },
+              ),
+              child: Hero(
+                tag: snapshot.data!.docs[index].id,
+                child: ProductCard(
+                  size: _size,
+                  index: index,
+                  product: product,
+                ),
+              ),
             );
           },
         );
