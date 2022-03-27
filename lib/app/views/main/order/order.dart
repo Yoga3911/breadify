7680921d9c20ea31431229935dd7ee3ea75f8f75
beatricstevzt17 <beatric.stevanyz@gmail.com>
2022-03-27 17:57:07 +0700

    rev

---
+++ lib/app/views/main/order/order.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:project/app/constant/glow.dart';
+
+class OrderPage extends StatelessWidget {
+  const OrderPage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
