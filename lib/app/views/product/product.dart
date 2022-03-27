3e6038cac4f76261471c002f71d4c71f3b0a318d
Robin Hood <yoga3911@gmail.com>
2022-03-27 17:55:13 +0700

    small changes

--- lib/app/views/product/product.dart
+++ lib/app/views/product/product.dart
@@ -1,8 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:project/app/constant/collection.dart';
 import 'package:project/app/view_model/category_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shimmer/shimmer.dart';
 
+import '../../constant/color.dart';
 import '../../models/product_model.dart';
 
 class ProductPage extends StatelessWidget {
@@ -15,6 +19,7 @@ class ProductPage extends StatelessWidget {
     final String _productId = _args["product_id"];
     final ProductModel _product = _args["product_data"];
     final _todayCategory = _args["category"];
+    final String _sellerId = _args["seller_id"];
     final Size _size = MediaQuery.of(context).size;
     return Container(
       color: Colors.white,
@@ -92,6 +97,47 @@ class ProductPage extends StatelessWidget {
                   ],
                 ),
               ),
+              FutureBuilder<DocumentSnapshot>(
+                  future: MyCollection.user.doc(_sellerId).get(),
+                  builder: (_, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Shimmer.fromColors(
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(shape: BoxShape.circle),
+                            ),
+                            Container(
+                              height: 30,
+                              width: 100,
+                            ),
+                          ],
+                        ),
+                        baseColor: MyColor.grey,
+                        highlightColor: MyColor.grey2,
+                        direction: ShimmerDirection.ltr,
+                      );
+                    }
+                    return Row(
+                      children: [
+                        Container(
+                          height: 100,
+                          width: 100,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: NetworkImage((snapshot.data!.data()
+                                      as Map<String, dynamic>)["image_url"]))),
+                        ),
+                        Container(
+                          height: 30,
+                          width: 100,
+                        ),
+                      ],
+                    );
+                  })
             ],
           ),
         ),
