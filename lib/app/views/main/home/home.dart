dbab5b51b7119b5ff8b2fc7f716d4bf7b8e24fda
beatricstevzt17 <beatric.stevanyz@gmail.com>
2022-03-29 07:03:36 +0700

    rev

--- lib/app/views/main/home/home.dart
+++ lib/app/views/main/home/home.dart
@@ -1,14 +1,13 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
-import 'package:project/app/services/facebook.dart';
-import 'package:project/app/view_model/user_prodvider.dart';
-import 'package:project/app/views/main/home/widgets/content.dart';
-import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:developer';
 
-import '../../../routes/route.dart';
-import '../../../services/google.dart';
+import 'package:flutter/material.dart';
+import 'package:project/app/views/main/home/widgets/alert.dart';
+import 'package:provider/provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import '../../../constant/collection.dart';
+import '../../main/home/widgets/content.dart';
+import '../../main/home/widgets/title.dart';
 import '../../../view_model/category_provider.dart';
 import '../../../views/main/home/widgets/category.dart';
 import '../../../views/main/home/widgets/header.dart';
@@ -17,44 +16,35 @@ import '../../../views/main/home/widgets/search.dart';
 import '../../../constant/glow.dart';
 import '../../../constant/color.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   void _logoutDialog(BuildContext context) async {
     showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (_) => AlertDialog(
-        title: const Text(
-          "Apakah kamu yakin ingin logout?",
-        ),
-        actions: [
-          ElevatedButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text("Batal"),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              final pref = await SharedPreferences.getInstance();
-              switch (pref.getString("social")) {
-                case "google":
-                  await GoogleService.signOut();
-                  break;
-                case "facebook":
-                  await FacebookService.signOut();
-                  break;
-              }
-              Navigator.pushNamedAndRemoveUntil(
-                context,
-                Routes.login,
-                (route) => false,
-              );
-            },
-            child: const Text("Yakin"),
-          ),
-        ],
-      ),
-    );
+        context: context,
+        barrierDismissible: false,
+        builder: (_) => const LogOutDialog());
+  }
+
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  void _onRefresh() async {
+    MyCollection.product.snapshots().listen((event) {
+      inspect(event.docs.first["name"]);
+    });
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
   }
 
   @override
@@ -63,6 +53,7 @@ class HomePage extends StatelessWidget {
         "https://firebasestorage.googleapis.com/v0/b/breadify-a4a04.appspot.com/o/user.png?alt=media&token=30e27068-d2ff-4dcb-b734-c818c49863fd";
     final _categoryProvider = Provider.of<CategoryProvider>(context);
     // final User _user = Provider.of<UserProvider>(context).getUser;
+
     return ScrollConfiguration(
       behavior: NoGlow(),
       child: GestureDetector(
@@ -73,41 +64,11 @@ class HomePage extends StatelessWidget {
             leading: IconButton(
               splashRadius: 1,
               onPressed: () {},
-              icon: CircleAvatar(
+              icon: const CircleAvatar(
                 backgroundImage: NetworkImage(_blank),
               ),
             ),
-            title: Container(
-              width: 170,
-              height: 45,
-              padding: const EdgeInsets.only(
-                left: 10,
-              ),
-              decoration: BoxDecoration(
-                color: MyColor.yellow,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    "Rp 100.000",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                  Text(
-                    "Click to see transaction history",
-                    style: TextStyle(
-                      color: MyColor.grey,
-                      fontSize: 10,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            title: const AppBarTitle(),
             actions: [
               IconButton(
                 onPressed: () {},
@@ -154,27 +115,39 @@ class HomePage extends StatelessWidget {
               color: Colors.white,
             ),
           ),
-          body: ListView(
-            children: [
-              const Header(),
-              const SearchBar(),
-              const ProductCategory(),
-              Container(
-                child: (_categoryProvider.getCategory != "Today")
-                    ? Container(
-                        color: Colors.white,
-                        padding: const EdgeInsets.only(left: 10, right: 10),
-                        child: Column(
-                          children: const [
-                            SizedBox(height: 10),
-                            Product(todayCategory: "Category"),
-                            SizedBox(height: 20),
-                          ],
-                        ),
-                      )
-                    : const HomeContent(),
-              ),
-            ],
+          body: SmartRefresher(
+            // enablePullUp: true,
+            header: const WaterDropHeader(),
+            controller: _refreshController,
+            onRefresh: _onRefresh,
+            onLoading: _onLoading,
+            child: ListView(
+              cacheExtent: 10000,
+              addAutomaticKeepAlives: false,
+              addRepaintBoundaries: false,
+              children: const [
+                Header(),
+                SearchBar(),
+                ProductCategory(),
+                SizedBox(height: 10),
+                Padding(
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: Product(todayCategory: "Popular"),
+                ),
+                SizedBox(height: 20),
+                // Container(
+                //   child: (_categoryProvider.getCategory != "Today")
+                //       ? Container(
+                //           color: Colors.white,
+                //           padding: const EdgeInsets.only(left: 10, right: 10),
+                //           child: Column(
+                //             children: const [],
+                //           ),
+                //         )
+                //       : const HomeContent(),
+                // ),
+              ],
+            ),
           ),
         ),
       ),
