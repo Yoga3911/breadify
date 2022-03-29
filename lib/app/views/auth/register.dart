981f1e73732f4d502180b2f9008d7089863fbc2e
112358Sean <seanthony99@gmail.com>
2022-03-29 15:50:11 +0700

    perbaikan noglow + tambahan favorite

--- lib/app/views/auth/register.dart
+++ lib/app/views/auth/register.dart
@@ -1,5 +1,6 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:project/app/constant/glow.dart';
 import 'package:project/app/routes/route.dart';
 import 'package:project/app/views/auth/login.dart';
 
@@ -9,237 +10,240 @@ class RegisterPage extends StatelessWidget {
   const RegisterPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          SizedBox(
-            child: Stack(
-              children: <Widget>[
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  width: MediaQuery.of(context).size.width * 1,
-                  child: const Image(
-                    image:
-                        AssetImage("assets/images/login_register_reverse.jfif"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.6,
-                  decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [Colors.transparent, Colors.white],
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
+    return ScrollConfiguration(
+      behavior: NoGlow(),
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            SizedBox(
+              child: Stack(
+                children: <Widget>[
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    width: MediaQuery.of(context).size.width * 1,
+                    child: const Image(
+                      image:
+                          AssetImage("assets/images/login_register_reverse.jfif"),
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(
-                      MediaQuery.of(context).size.height * 0.05,
-                      MediaQuery.of(context).size.height * 0.46,
-                      MediaQuery.of(context).size.height * 0.05,
-                      MediaQuery.of(context).size.height * 0.05),
-                  child: const AutoSizeText(
-                    "Sign Up",
-                    maxLines: 1,
-                    style:
-                        TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                        colors: [Colors.transparent, Colors.white],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                      ),
+                    ),
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(
-                      MediaQuery.of(context).size.height * 0.05,
-                      MediaQuery.of(context).size.height * 0.55,
-                      MediaQuery.of(context).size.height * 0.05,
-                      MediaQuery.of(context).size.height * 0.05),
-                  child: const AutoSizeText(
-                    "Create Your Breadify's Account",
-                    maxLines: 1,
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: MyColor.yellow,
-                        fontWeight: FontWeight.w500),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-                MediaQuery.of(context).size.height * 0.05,
-                30,
-                MediaQuery.of(context).size.height * 0.05,
-                0),
-            child: TextField(
-              decoration: InputDecoration(
-                // icon: Icon(Icons.account_box),
-                prefixIcon: const Icon(Icons.person),
-                prefixStyle: const TextStyle(color: Colors.blue),
-                hintText: "Username",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-                MediaQuery.of(context).size.height * 0.05,
-                30,
-                MediaQuery.of(context).size.height * 0.05,
-                0),
-            child: TextField(
-              decoration: InputDecoration(
-                // icon: Icon(Icons.account_box),
-                prefixIcon: const Icon(Icons.email_outlined),
-                prefixStyle: const TextStyle(color: Colors.blue),
-                hintText: "Email",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-                MediaQuery.of(context).size.height * 0.05,
-                30,
-                MediaQuery.of(context).size.height * 0.05,
-                0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                // icon: Icon(Icons.account_box),
-                prefixIcon: const Icon(Icons.lock_outline),
-                suffixIcon: const Icon(Icons.remove_red_eye_outlined),
-                prefixStyle: const TextStyle(color: Colors.blue),
-                hintText: "Password",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-                MediaQuery.of(context).size.height * 0.05,
-                30,
-                MediaQuery.of(context).size.height * 0.05,
-                0),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                // icon: Icon(Icons.account_box),
-                prefixIcon: const Icon(Icons.lock_outline),
-                suffixIcon: const Icon(Icons.remove_red_eye_outlined),
-                prefixStyle: const TextStyle(color: Colors.blue),
-                hintText: "Confirm Password",
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-              ),
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(
-                MediaQuery.of(context).size.height * 0.1,
-                50,
-                MediaQuery.of(context).size.height * 0.1,
-                30),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: MyColor.yellow,
-                padding: const EdgeInsets.all(10),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-              onPressed: () {},
-              child: const Text("Sign Up",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w900)),
-            ),
-          ),
-          Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text("ALREADY HAVE ACCOUNT? ",
-                      textAlign: TextAlign.center,
+                  Container(
+                    margin: EdgeInsets.fromLTRB(
+                        MediaQuery.of(context).size.height * 0.05,
+                        MediaQuery.of(context).size.height * 0.46,
+                        MediaQuery.of(context).size.height * 0.05,
+                        MediaQuery.of(context).size.height * 0.05),
+                    child: const AutoSizeText(
+                      "Sign Up",
+                      maxLines: 1,
                       style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushNamed(context, Routes.login);
-                    },
-                    child: const Text(
-                      "SIGN UP HERE!",
-                      textAlign: TextAlign.center,
+                          TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(
+                        MediaQuery.of(context).size.height * 0.05,
+                        MediaQuery.of(context).size.height * 0.55,
+                        MediaQuery.of(context).size.height * 0.05,
+                        MediaQuery.of(context).size.height * 0.05),
+                    child: const AutoSizeText(
+                      "Create Your Breadify's Account",
+                      maxLines: 1,
                       style: TextStyle(
-                          fontSize: 18,
+                          fontSize: 30,
                           color: MyColor.yellow,
                           fontWeight: FontWeight.w500),
                     ),
                   ),
                 ],
               ),
-              Container(
-                margin: const EdgeInsets.all(40),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: 2,
-                      width: 50,
-                      decoration: const BoxDecoration(color: MyColor.grey2),
-                    ),
-                    const Text(
-                      "OR    ",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: MyColor.grey2,
-                          fontWeight: FontWeight.w500),
-                    ),
-                    Container(
-                      height: 2,
-                      width: 50,
-                      decoration: const BoxDecoration(color: MyColor.grey2),
-                    ),
-                  ],
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.height * 0.05,
+                  30,
+                  MediaQuery.of(context).size.height * 0.05,
+                  0),
+              child: TextField(
+                decoration: InputDecoration(
+                  // icon: Icon(Icons.account_box),
+                  prefixIcon: const Icon(Icons.person),
+                  prefixStyle: const TextStyle(color: Colors.blue),
+                  hintText: "Username",
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 50),
-                child: Row(
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.height * 0.05,
+                  30,
+                  MediaQuery.of(context).size.height * 0.05,
+                  0),
+              child: TextField(
+                decoration: InputDecoration(
+                  // icon: Icon(Icons.account_box),
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  prefixStyle: const TextStyle(color: Colors.blue),
+                  hintText: "Email",
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.height * 0.05,
+                  30,
+                  MediaQuery.of(context).size.height * 0.05,
+                  0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  // icon: Icon(Icons.account_box),
+                  prefixIcon: const Icon(Icons.lock_outline),
+                  suffixIcon: const Icon(Icons.remove_red_eye_outlined),
+                  prefixStyle: const TextStyle(color: Colors.blue),
+                  hintText: "Password",
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.height * 0.05,
+                  30,
+                  MediaQuery.of(context).size.height * 0.05,
+                  0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  // icon: Icon(Icons.account_box),
+                  prefixIcon: const Icon(Icons.lock_outline),
+                  suffixIcon: const Icon(Icons.remove_red_eye_outlined),
+                  prefixStyle: const TextStyle(color: Colors.blue),
+                  hintText: "Confirm Password",
+                  border:
+                      OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(
+                  MediaQuery.of(context).size.height * 0.1,
+                  50,
+                  MediaQuery.of(context).size.height * 0.1,
+                  30),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: MyColor.yellow,
+                  padding: const EdgeInsets.all(10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text("Sign Up",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w900)),
+              ),
+            ),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    const Spacer(
-                      flex: 8,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Image(
-                        image: AssetImage("assets/images/google.png"),
+                    const Text("ALREADY HAVE ACCOUNT? ",
+                        textAlign: TextAlign.center,
+                        style:
+                            TextStyle(fontSize: 18, fontWeight: FontWeight.w500)),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, Routes.login);
+                      },
+                      child: const Text(
+                        "SIGN UP HERE!",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: MyColor.yellow,
+                            fontWeight: FontWeight.w500),
                       ),
-                    ),
-                    const Spacer(
-                      flex: 3,
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Image(
-                        image: AssetImage("assets/images/facebook.png"),
-                      ),
-                    ),
-                    const Spacer(
-                      flex: 8,
                     ),
                   ],
                 ),
-              )
-            ],
-          ),
-        ],
+                Container(
+                  margin: const EdgeInsets.all(40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 2,
+                        width: 50,
+                        decoration: const BoxDecoration(color: MyColor.grey2),
+                      ),
+                      const Text(
+                        "OR    ",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: MyColor.grey2,
+                            fontWeight: FontWeight.w500),
+                      ),
+                      Container(
+                        height: 2,
+                        width: 50,
+                        decoration: const BoxDecoration(color: MyColor.grey2),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.fromLTRB(0, 0, 0, 50),
+                  child: Row(
+                    children: <Widget>[
+                      const Spacer(
+                        flex: 8,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Image(
+                          image: AssetImage("assets/images/google.png"),
+                        ),
+                      ),
+                      const Spacer(
+                        flex: 3,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Image(
+                          image: AssetImage("assets/images/facebook.png"),
+                        ),
+                      ),
+                      const Spacer(
+                        flex: 8,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/app/views/main/favorite/favorite.dart
+++ lib/app/views/main/favorite/favorite.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:project/app/constant/glow.dart';
 
 class FavoritePage extends StatelessWidget {
   const FavoritePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Favorite"),
+    return ScrollConfiguration(
+      behavior: NoGlow(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Favorite"),
+          actions: [
+            IconButton(
+              onPressed: (){}, 
+              icon: Icon(Icons.shopping_cart,
+              color:Colors.white),
+            )
+          ],
+        ),
+        body: ListView(
+          children: [
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                            MediaQuery.of(context).size.width*0.025, 
+                            MediaQuery.of(context).size.width*0.025, 
+                            MediaQuery.of(context).size.width*0.025, 
+                            MediaQuery.of(context).size.width*0.025),
+                          height: MediaQuery.of(context).size.height*0.4,
+                          width: MediaQuery.of(context).size.width*0.45,
+                        ),
+                        Container(
+                          margin: EdgeInsets.fromLTRB(
+                            MediaQuery.of(context).size.width*0.025, 
+                            MediaQuery.of(context).size.width*0.025, 
+                            MediaQuery.of(context).size.width*0.025, 
+                            MediaQuery.of(context).size.width*0.025),
+                          height: MediaQuery.of(context).size.height*0.4,
+                          width: MediaQuery.of(context).size.width*0.45,
+                        ),
+                      ],
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
