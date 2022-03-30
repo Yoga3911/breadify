d882f4cd92207fe54d5c6d8b231c4bb0def2e5f8
112358Sean <seanthony99@gmail.com>
2022-03-30 18:01:20 +0700

    nyoba pull

--- lib/app/views/auth/login.dart
+++ lib/app/views/auth/login.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:project/app/view_model/login_provider.dart';
 import 'package:project/app/view_model/user_prodvider.dart';
+import 'package:project/app/widgets/custom_loading.dart';
 import 'package:provider/provider.dart';
 
 import '../../constant/color.dart';
