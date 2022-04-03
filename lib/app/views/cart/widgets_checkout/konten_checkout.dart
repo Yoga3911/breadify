77d4b0ff1ff39656803d0c6cd4063c4e7b0948bf
beatricstevzt17 <beatric.stevanyz@gmail.com>
2022-04-03 18:37:40 +0700

    isi konten checkout page

--- lib/app/views/cart/widgets_checkout/konten_checkout.dart
+++ lib/app/views/cart/widgets_checkout/konten_checkout.dart
@@ -9,10 +9,38 @@ class Checkout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
       width: MediaQuery.of(context).size.width * 1,
-      height: MediaQuery.of(context).size.height * 0.08,
-      color: Colors.amber,
+      height: MediaQuery.of(context).size.height * 0.15,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.grey),
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          //1) anak ke-1 : gambar produk
+          Container(
+            width: MediaQuery.of(context).size.width * 0.3,
+            color: Colors.red,
+          ),
+          const SizedBox(width: 4),
+
+          //2) column : for text
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              //2a) product name
+              Text(checkoutcontent[index].productName),
+              //2b) bakery
+              Text(checkoutcontent[index].bakery),
+              //2c) price
+              Text(checkoutcontent[index].price)
+            ],
+          )
+        ],
+      ),
     );
   }
 }
