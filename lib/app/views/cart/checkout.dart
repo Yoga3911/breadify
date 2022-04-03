b7908f8c6ab5168333d88d678d2d4c3ddb7023df
beatricstevzt17 <beatric.stevanyz@gmail.com>
2022-04-03 17:21:52 +0700

    appbar checkout

--- lib/app/views/cart/checkout.dart
+++ lib/app/views/cart/checkout.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:project/app/constant/glow.dart';
+
+//pemanggilan class
+// import '../../models/notif_models.dart';
+
+//utk konten Notif :
+// import 'package:project/app/views/cart/widgets_notif/konten_notif.dart';
 
 class CheckoutPage extends StatelessWidget {
-  const CheckoutPage({ Key? key }) : super(key: key);
+  const CheckoutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return ScrollConfiguration(
+      behavior: NoGlow(),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.arrow_back_ios_new,
+                color: Colors.white,
+              )),
+          title: const Text(
+            "Checkout",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
     );
   }
 }
