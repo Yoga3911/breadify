36acd1d9463c7bfa84929cbd44b7c704f66a9e23
beatricstevzt17 <beatric.stevanyz@gmail.com>
2022-04-07 17:51:25 +0700

    completing checkout

--- lib/app/views/cart/checkout.dart
+++ lib/app/views/cart/checkout.dart
@@ -48,118 +48,130 @@ class CheckoutPage extends StatelessWidget {
     return ScrollConfiguration(
       behavior: NoGlow(),
       child: Scaffold(
-          floatingActionButton: Container(
-            height: 48,
-            width: 130,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: FittedBox(
-              child: FloatingActionButton(
-                splashColor: Colors.transparent,
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                onPressed: () {},
-                child: const Text(
-                  "Order",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold),
-                ),
+        floatingActionButton: Container(
+          height: 48,
+          width: 130,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: FittedBox(
+            child: FloatingActionButton(
+              splashColor: Colors.transparent,
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              onPressed: () {},
+              child: const Text(
+                "Order",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
               ),
             ),
           ),
-          appBar: AppBar(
-            leading: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.arrow_back_ios_new,
-                  color: Colors.white,
-                )),
-            title: const Text(
-              "Checkout",
-              style:
-                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back_ios_new,
+              color: Colors.white,
             ),
           ),
-          body: ListView(
-            children: <Widget>[
-              //1) utk alamat checkout
-              Container(
-                height: MediaQuery.of(context).size.width * 0.1,
-                color: const Color(0xFFB4B4B4),
-                child: Row(
-                  children: <Widget>[
-                    //a) konten 1 : untuk icon gps & column for text
-                    Row(
-                      children: <Widget>[
-                        //a1) icon GPS
-                        Transform.scale(
-                          scale: 0.7,
-                          child: Image.asset("assets/icons/map.png"),
-                        ),
-                        //a2) column for text
-                        Column(
-                          children: <Widget>[
-                            //judul : address
-                            const Text(
-                              "Address",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.black,
-                                  fontSize: 15),
-                            ),
-                            //nama
-                            Text(
-                              address[i].nama,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
-                                  fontSize: 15),
-                            ),
-                            //telpon
-                            Text(
-                              address[i].telpon,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
-                                  fontSize: 15),
-                            ),
-                            //alamat
-                            Text(
-                              address[i].alamat,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
-                                  fontSize: 15),
-                            ),
-                            //kabupaten
-                            Text(
-                              address[i].kabupaten,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.grey,
-                                  fontSize: 15),
-                            ),
-                          ],
-                        )
-                      ],
+          title: const Text(
+            "Checkout",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: ListView(
+          children: <Widget>[
+            //1) utk alamat checkout
+            Container(
+              height: MediaQuery.of(context).size.width * 0.1,
+              color: const Color(0xFFB4B4B4),
+              child: Row(
+                children: <Widget>[
+                  //a) konten 1 : untuk icon gps & column for text
+                  Row(
+                    children: <Widget>[
+                      //a1) icon GPS
+                      Transform.scale(
+                        scale: 0.7,
+                        child: Image.asset("assets/icons/map.png"),
+                      ),
+                      //a2) column for text
+                      Column(
+                        children: <Widget>[
+                          //judul : address
+                          const Text(
+                            "Address",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                                fontSize: 15),
+                          ),
+                          //nama
+                          Text(
+                            address[i].nama,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                fontSize: 15),
+                          ),
+                          //telpon
+                          Text(
+                            address[i].telpon,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                fontSize: 15),
+                          ),
+                          //alamat
+                          Text(
+                            address[i].alamat,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                fontSize: 15),
+                          ),
+                          //kabupaten
+                          Text(
+                            address[i].kabupaten,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                fontSize: 15),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  //b) konten 2 : button "change"
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Transform.scale(
+                      scale: 0.7,
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.arrow_forward_ios),
+                        color: Colors.black,
+                      ),
                     ),
-                    //b) konten 2 : button "change"
-                  ],
-                ),
+                  ),
+                ],
               ),
-              //2) utk konten perulangan
-              ListView.builder(
-                itemCount: checkoutcontent.length,
-                itemBuilder: (context, index) => Checkout(index: index),
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-              ),
-            ],
-          )),
+            ),
+            //2) utk konten perulangan
+            ListView.builder(
+              itemCount: checkoutcontent.length,
+              itemBuilder: (context, index) => Checkout(index: index),
+              physics: const NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
